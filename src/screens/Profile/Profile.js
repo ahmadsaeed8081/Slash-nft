8e29dbbe4e4db133cb160ee53849852e58a282f4
ahmad <ahmadsaeed8081@gmail.com>
2023-10-26 09:24:49 +0500

    image updtaion error solved add

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -51,18 +51,21 @@ async function upload()
   const res0 =await axios.get("https://slashapi-production.up.railway.app/get?"+ new URLSearchParams({
     userAddress: address,}));
 
-  if(res0.data[0].image)
+  if(res0.data[0]!=undefined)
   {
+
     const data={userAddress:address, Name: name,image:preview}
     const res =await axios.patch("https://slashapi-production.up.railway.app/user/"+ res0.data[0]._id,data);
 
       console.log(res)
-
+    alert("Profile is updated")
   }
   else{
     const data={userAddress:address, Name: name,image:preview}
 
     const res =await axios.post("https://slashapi-production.up.railway.app/add",data)
+    alert("Profile is updated")
+
   }
 
 
@@ -72,14 +75,19 @@ async function get_data()
 {
 
 
-  const res =await axios.get("https://slashapi-production.up.railway.app/get?"+ new URLSearchParams({
-    userAddress: address,}))
+    const res =await axios.get("https://slashapi-production.up.railway.app/get?"+ new URLSearchParams({
+      userAddress: address}))
+      alert(res)
+      if(res.data[0]!=undefined)
+      {
+    setPreview(res.data[0].image);
+    setName(res.data[0].Name)
+  }
 
-  setPreview(res.data[0].image);
-  setName(res.data[0].Name)
 
 
 
+  
 }
 
   return (
